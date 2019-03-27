6ecae9e21b3c1c91b55ef815a7defe2b99ac5240
asqscdhcp666 <lukas.petracek@educanet.cz>
2019-03-27 01:08:03 +0100

    value check

--- PiCalc.java
+++ PiCalc.java
@@ -2,14 +2,28 @@ import java.math.BigDecimal;
 
 class PiCalc {
     public static void main(String[] args) {
-
+        
         BigDecimal pi = new BigDecimal("0");
+        BigDecimal tmpi = new BigDecimal("0");
+        BigDecimal check = new BigDecimal("0");
+        BigDecimal checktemp = new BigDecimal("0");
+        BigDecimal rekordpi = new BigDecimal("0");
+        BigDecimal oficpi = new BigDecimal("3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862");
+        long i = 0;
 
-        for (long i = 0;; i++) {
+        for (;; i++) {
 
-            pi = pi.add(BigDecimal.valueOf(Math.pow(-1, i) / (2 * i + 1)));  
+            pi = pi.add(BigDecimal.valueOf(Math.pow(-1, i) / (2 * i + 1)));
 
-            System.out.println(i + " | " + pi.multiply(BigDecimal.valueOf(4)));    
+            tmpi = pi.multiply(BigDecimal.valueOf(4));
+
+            check = oficpi.subtract(tmpi);
+            if (check.compareTo(checktemp) < 0) {
+                rekordpi = tmpi;
+                System.out.print("\r" + i + " | " + rekordpi);
+            }
+            checktemp = check;
+
         }
     }
 }
